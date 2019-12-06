e880bbdfc1878356ecd5fc306ebcd3c9fdefcc9f
Simon Phillips <simonxphillips@gmail.com>
2019-12-06 13:56:35 -0500

    Setup project validation

--- PPMToolFullStack/src/main/java/io/simonxphillips/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/simonxphillips/ppmtool/domain/Project.java
@@ -1,7 +1,11 @@
 package io.simonxphillips.ppmtool.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,17 +14,25 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min=4, max=5, messgae = "Please use 4 or 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date updated_At;
 
     public Project() {
-
     }
 
     public Long getId() {
