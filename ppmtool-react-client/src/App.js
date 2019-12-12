12b0dc575dcc7ae568b40fc947c57ead5ab18687
Simon Phillips <simonxphillips@gmail.com>
2019-12-12 14:45:47 -0500

    Created first complete UI, added Bootstrap and FA

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
+        <Header />
         <Dashboard />
       </div>
     );
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -4,14 +4,22 @@ import ProjectItem from "./Project/ProjectItem";
 class Dashboard extends Component {
   render() {
     return (
-    <div>
-      <h1>Welcome to the Dashboard</h1>
-      <ProjectItem />
-      <ProjectItem />
-
-      <ProjectItem />
+      <div class="projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="display-4 text-center">Projects</h1>
+                    <br />
+                    <a href="ProjectForm.html" class="btn btn-lg btn-info">
+                        Create a Project
+                    </a>
+                    <br />
+                    <hr />
+                    <ProjectItem />
+                </div>
+            </div>
+        </div>
     </div>
-    
     );
   }
 }
